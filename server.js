1c14ea107f85b5111f1bc92dda071617d7c3f4e5
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-01-10 18:21:34 -0800

    added :first: first GET route

--- server.js
+++ server.js
@@ -2,4 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const mockUserData = [
+  { name: 'firecracker' },
+  { name: 'rosaline' },
+];
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Successful got users. Nice!',
+    data: mockUserData,
+  });
+});
+
 app.listen(8000, () => console.log('Server is running'));
